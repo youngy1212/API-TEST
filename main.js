d443a3b2389c7686225fa6a21da02355bb4bb88e
jiyoung yun <letea8018@gmail.com>
2023-04-03 18:52:30 +0900

    commit

--- main.js
+++ main.js
@@ -1,64 +1,120 @@
 
-let news = []
+//ruQg4pza-3YsUgG4ZAv6kKwY8a6W70GGGIvldcNqnzw 키 
+const API_KEY = "ruQg4pza-3YsUgG4ZAv6kKwY8a6W70GGGIvldcNqnzw"; 
+
+//변수 선언
+let articles = []; // API를 받아올 배열
+let page = 1; 
+let total_pages = 1;
+
+//기본으로 들어갈 URL
+let url = new URL( //전역변수로 선언 : 계속변경
+    "https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10"
+)
+
 let menus = document.querySelectorAll(".menus button")
 menus.forEach(menu => menu.addEventListener("click", (event)=> getNewsByTopic(event)))
-let searchButton = document.getElementById("search-button")
 
 
-const getLatesNews = async ()=>{
-    //주소URL 
-    let url= new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10`);
-    //header로 정보 넘겨주기
-    let header = new Headers({"x-api-key":"ruQg4pza-3YsUgG4ZAv6kKwY8a6W70GGGIvldcNqnzw"})
+//코드 리팩토링
+//각 함수에서 필요한 url을 만든다 
+//api 호출함수를 부른다
+const getNews = async () => {
+    try {
+        let header = new Headers();
+        header.append("x-api-key",API_KEY);
+
+    //URL에 쿼리 더하기
+    url.searchParams.set('page', page) //&page= 
+
     //자료를 받음
-    let response = await fetch(url, {headers: header});
+    let response = await fetch(url, { headers: header });
+    //header로 정보 넘겨주기
     let data = await response.json();
-    news = data.articles
-    
-    console.log(data);
-    console.log(news);
+    if(response.status == 200){
+        if(data.total_hit == 0){
+            console.log("A", data);
+            page = 0;
+            total_pages = 0;
+            renderPagenation();
+            throw new Error("검색된 결과값이 없습니다")
+            //에러가 날 경우 여기서 멈추기 떄문에 아래는 실행X
+        }
 
-    render();
+        console.log("B", data);
+        articles = data.articles;
+        console.log(articles);
+        total_pages = data.total_pages; // 페이지네이션을 위한 값 설정
+        page = data.page;
+        render();
+        renderPagenation(); 
+    }else{
+        page = 0;
+        total_pages = 0;
+        renderPagenation(); 
+        throw new Error(data.message);
+    }
+
+    }catch(error){
+        console.log("에러 : ", error.message)
+        errorRender(error.message);
+        page = 0;
+        total_pages = 0;
+        renderPagenation();
+    }
+
+   
+    console.log("response : ",response);
+    console.log("data : ",data);
+};
+
+const getLatestNews =  ()=>{
+    //주소URL 
+    url = new URL(
+        `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10`
+      );
+    page = 1; // 새로운 search 마다 1 로 리셋
+    getNews();
 
 };
 
 const getNewsByTopic = async (event) => {
     //console.log("클릭됨", event.target.textContent); //taraget <butto>~~</butto> //내용만 : textcontent
     let topic = event.target.textContent.toLowerCase()
-    let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10&topic=${topic}`)
+    url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10&topic=${topic}`)
     //console.log(url);
-    let header = new Headers({"x-api-key":"ruQg4pza-3YsUgG4ZAv6kKwY8a6W70GGGIvldcNqnzw"})
-    let response = await fetch(url, {headers: header});
-    let data = await response.json();
-    news=data.articles
-    render()
+    getNews();
 }
 
-const getNewsBykeyword = async() => {
+const openSearchBox = () =>{
+    let inputArea = document.getElementById("input-area");
+    if(inputArea.style.display ==="inline"){
+        inputArea.style.display = "none";
+    }else{
+        inputArea.style.display = "inline"
+    }
+};
+
+
+
+const searchNews  = async() => {
     //1. 검색키워드 읽어오기
     let keyword = document.getElementById("search-input").value;
-
+    page = 1;
     //2. url에 검색키워드 부치기
-    let url = new URL(`https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=10`)
-
-    //3. 헤더준비
-    let header = new Headers({"x-api-key":"ruQg4pza-3YsUgG4ZAv6kKwY8a6W70GGGIvldcNqnzw"})
-
-    //4. url부르기
-    let response = await fetch(url, {headers: header});
-    //5. 데이터 가져오기
-    let data = await response.json();
-    //6. 데이터 보여주기
-    news=data.articles
-    render()
-}
+    url = new URL(
+        `https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=10`
+      );
+    getNews();
+   
+};
 
 //join string으로 array를 바꿔줌
 
 const render = () => {
     let newsHTML = "";
 
-    newsHTML = news.map((item)=>{ 
+    newsHTML = articles.map((item)=>{ 
         return  `<div class="row news">
                     <div class="col-lg-4">
                         <img class="new-img-size" src="${item.media || 
@@ -77,7 +133,7 @@ const render = () => {
                         </div>
                     </div>
                 </div>`
-    }).join('');
+    }).join("");
 
     console.log(newsHTML);
 
@@ -85,10 +141,15 @@ const render = () => {
     document.getElementById("new-board").innerHTML = newsHTML;
 }
 
-searchButton.addEventListener("click", getNewsBykeyword )
-getLatesNews();
 
-//ruQg4pza-3YsUgG4ZAv6kKwY8a6W70GGGIvldcNqnzw 키 
+
+
+
+// let searchButton = document.getElementById("search-button")
+// searchButton.addEventListener("click", getNewsBykeyword )
+// getLatesNews();
+
+
 
 const closeNav = () => {
     document.getElementById("mySidenav").style.width = "0"
@@ -98,11 +159,76 @@ const openNav = () =>{
     document.getElementById("mySidenav").style.width = "250px"
 }
 
-const openSearchBox = () =>{
-    let inputArea = document.getElementById("input-area");
-    if(inputArea.style.display ==="inline"){
-        inputArea.style.display = "none";
-    }else{
-        inputArea.style.display = "inline"
+
+/////////////////페이지 네이션
+const renderPagenation = () => {
+    // 1.1~5까지를 보여준다
+    // 2.6~10을 보여준다 => last, first 가필요
+    // 3.만약에 first가 6 이상이면 prev 버튼을 단다
+    // 4.만약에 last가 마지막이 아니라면 next버튼을 단다
+    // 5.마지막이 5개이하이면 last=totalpage이다
+    // 6.페이지가 5개 이하라면 first = 1이다
+    let pagenationHTML  = `` //htmlpage를 담아줄 변수
+
+    //total_page : API 키 
+    //page : API 키
+    //page group
+    let pageGrop = Math.ceil(page/5)
+    //last 
+    let last = pageGrop*5
+    if (last > total_pages) {
+        // 마지막 그룹이 5개 이하이면
+        last = total_pages;
+      }
+    //first
+    let first = last -4 <= 0? 1 : last -4; //첫그룹이 5이하면
+    //first ~ last : 페이지 프린트
+    if (first >= 6) {
+        pagenationHTML = `<li class="page-item" onclick="pageClick(1)">
+                            <a class="page-link" href='#js-bottom'>&lt;&lt;</a>
+                          </li>
+                          <li class="page-item" onclick="pageClick(${page - 1})">
+                            <a class="page-link" href='#js-bottom'>&lt;</a>
+                          </li>`;
     }
+
+    for(let i= first; i<= last; i++){
+        pagenationHTML +=  `<li class="page-item ${page==i}? "active": "" ">
+        <a class="page-link" href="#" onclick="moveToPage(${i})">${i}</a></li>`
+    }
+
+
+    if (last < total_pages) {
+        pagenationHTML += `<li class="page-item" onclick="pageClick(${page + 1})">
+                            <a  class="page-link" href='#js-program-detail-bottom'>&gt;</a>
+                           </li>
+                           <li class="page-item" onclick="pageClick(${totalPage})">
+                            <a class="page-link" href='#js-bottom'>&gt;&gt;</a>
+                           </li>`;
+      }
+    
+
+    document.querySelector(".pagination").innerHTML = pagenationHTML;
+
+};
+
+
+
+const moveToPage = (pageNum)=> {
+    //클릭이벤트 세팅
+    //이동하고싶은 페이지 알아오기
+    page = pageNum
+    //이동하고싶은 페이지를 가지고 api를 다시 호출
+    
+    window.scroll({top:0, behavior: "smooth"})
+    getNews();
+
 }
+
+const errorRender = (message) => {
+    let errorHTML = `<h3 class="alert alert-danger" role="alert">${message}</h3>`;
+    document.getElementById("new-board").innerHTML = errorHTML;
+}
+
+
+getLatestNews(); //호출
